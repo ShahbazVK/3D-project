887f44b05cb5ade20bc872dc77fb41843656ca68
Shahbaz Ali <shahbazalivk@gmail.com>
2023-12-03 01:27:42 +0500

    keys and mouse touch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 };
 
