40545187152edb83b219e558b34ddd2ae2ac4f21
Shahbaz Ali <shahbazalivk@gmail.com>
2023-12-05 00:54:42 +0500

    completed

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -42,9 +42,11 @@ const Island = ({ isRotating, setisRotating, setCurrentStage, ...props }) => {
     if (e.key === "ArrowLeft") {
       if (!isRotating) setisRotating(true);
       islandRef.current.rotation.y += 0.01 * Math.PI;
+      rotationSpeed.current = 0.0125;
     } else if (e.key === "ArrowRight") {
       if (!isRotating) setisRotating(true);
       islandRef.current.rotation.y -= 0.01 * Math.PI;
+      rotationSpeed.current = -0.0125;
     }
   };
 
